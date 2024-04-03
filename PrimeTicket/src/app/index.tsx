0f40f1e5392c01dcfb0722c728e37fa4729037f9
Peu <pedrocavalcanti.it@gmail.com>
2024-04-03 14:13:53 -0300

    Criação de documentação base

---
+++ PrimeTicket/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+
+    return (
+
+        <View style={{ backgroundColor: "beige", flex: 1}}>
+            <Text>Iníciando...</Text>
+        </View>
+    )
+}
