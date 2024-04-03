eaef4991450b1dc26f4142d45b8bcb7341d27500
Peu <pedrocavalcanti.it@gmail.com>
2024-04-03 15:41:37 -0300

    tela inicial com primeiro input

--- PrimeTicket/src/app/index.tsx
+++ PrimeTicket/src/app/index.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+
+import { Input } from "@/components/input";
+import { colors } from "@/styles/colors";
 
 export default function Home() {
 
     return (
 
-        <View className="flex-1 bg-green-500 items-center justify-center">
-            <Text className="text-white">Iníciando...</Text>
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain" />
+
+            <View className="w-full mt-12 gap-3">
+                <Input>
+                    <MaterialCommunityIcons name="ticket-confirmation-outline" size={20} color={colors.green[200]} />
+                    <Input.Field placeholder="Código do ingresso" />
+                </Input>
+            </View>
+
+
         </View>
     )
 }
