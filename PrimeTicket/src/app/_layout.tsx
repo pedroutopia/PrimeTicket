a96dc0a6e06758ab453d6eb916b56114a49af54e
Peu <pedrocavalcanti.it@gmail.com>
2024-04-03 15:05:05 -0300

    adicionando fonts do google-fonts

--- PrimeTicket/src/app/_layout.tsx
+++ PrimeTicket/src/app/_layout.tsx
@@ -1,8 +1,33 @@
 import "../styles/global.css";
 
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import {
+    useFonts,
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular
+} from "@expo-google-fonts/roboto";
 
 export default function Layout() {
 
-    return <Slot />
+    const[fontsLoaded] = useFonts({
+        Roboto_700Bold,
+        Roboto_500Medium,
+        Roboto_400Regular,
+    })
+
+    if(!fontsLoaded) {
+        return
+    }
+
+    return (
+
+        <>
+            <StatusBar style="light" />
+            <Slot />
+        </>
+
+    )
 }
